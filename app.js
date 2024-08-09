72b72828e2228a830b0d09ff1283eb853d531d73
dorohovgeorge <dorohovgeorge@gmail.com>
2024-08-09 17:19:54 +0300

    rewrite fetch funcitons

--- app.js
+++ app.js
@@ -2,40 +2,32 @@ const express = require('express')
 const app = express()
 var port = 3000
 const fetch = (...args) =>
-	import('node-fetch').then(({default: fetch}) => fetch(...args));
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-const url = 'http://vpn.94.103.89.23.sslip.io/'
+const url = 'http://localhost:8090/'
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.append('Access-Control-Allow-Headers', ['Content-Type', 'Authorization']);
-    res.append('Access-Control-Allow-Credentials', true)
-    res.append('Access-Control-Expose-Headers', ['*'])
-    res.append('content-type', 'text/plain')
-    req.connection.setTimeout(2*60*1000)
-    next();
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.append('Access-Control-Allow-Headers', ['Content-Type', 'Authorization']);
+  res.append('Access-Control-Allow-Credentials', true)
+  res.append('Access-Control-Expose-Headers', ['*'])
+  res.append('content-type', 'text/plain')
+  req.connection.setTimeout(60 * 1000)
+  next();
 });
 
 app.get('/key/:id', async (req, res) => {
-    const id = parseInt(req.params.id)
-    const options = {
-		method: 'GET'
-	};
-    var str = url+id
-    console.log(str)
-    fetch(str, options)
-		.then(res => res.text())
-		.catch(err => console.error('error:' + err));
+  const id = parseInt(req.params.id)
+  const options = {
+    method: 'GET'
+  };
+  var str = url + id
+  console.log(str)
 
-    	try {
-            let response = await fetch(str, options);
-            let ans = await response.text();
-            res.send(ans)
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({msg: `Internal Server Error.`});
-        }    
+  await fetch(str, options).then(
+    async out => res.send(await out.text())
+  ).catch(err => res.status(500).json({ msg: `Internal Error` }))
 })
 
 // app.get('/allConnections', async (req, res) => {
@@ -49,7 +41,7 @@ app.get('/key/:id', async (req, res) => {
 //             .then(res => res.text())
 //             .then(text => console.log(text))
 //             .catch(err => console.error('error:' + err));
-    
+
 //             try {
 //                 let response = await fetch(str, options);
 //                 response = await response.text();
